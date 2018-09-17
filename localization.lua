76ac61e40ea08db3894d4768d468cf286aba5b31
Zaggy1 <42075788+Zaggy1@users.noreply.github.com>
2018-09-17 14:34:12 +0200

    Update localization.lua

--- localization.lua
+++ localization.lua
@@ -133,6 +133,7 @@ elseif (GetLocale() == "deDE") then -- German
 	TB_tauntLine = "Ihr habt es mit Spott versucht, aber (.+) hat widerstanden.";
 	TB_growlLine = "Ihr habt es mit Knurren versucht, aber (.+) hat widerstanden."; 
 	TB_mbHitLine = "Sp\195\182ttischer Schlag von Euch trifft (.+) f\195\188r (.+)"
+	TB_mbCrit = "Sp\195\182ttischer Schlag trifft (.+) kritisch: (.+)"
 	TB_mb = "(.*)Sp\195\182ttischer Schlag(.*)";
 	TB_ls = "Ihr bekommt 'Letztes Gefecht'."; 
 	TB_sw = "Ihr bekommt 'Schildwall'."; 
